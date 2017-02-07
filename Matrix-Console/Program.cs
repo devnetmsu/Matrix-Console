ea7fe189fadbd2e4ffa7156d096c39f561678421
Evan Dixon <evanldixon@live.com>
2017-02-06 20:44:28 -0600

    Added an application

---
+++ Matrix-Console/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrix_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Matrix Console");
+            Console.WriteLine();
+
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+
+            var r = new Random();
+            while (true)
+            {
+                // To-Do: make background color random
+                //Console.ForegroundColor = (ConsoleColor)r.Next(0, 15);
+
+                // Write a random character
+                Console.Write((char)r.Next(20, 255));
+            }
+        }
+    }
+}
